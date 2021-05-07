347714e5314f1f18b201c1f02d4d9bdf01e1b5a8
Viktoriya Paul <>
2021-05-07 14:12:06 +0000

    new

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ document.getElementById('button').addEventListener('click', randomizer)
 let randomNumber = 0
 let userGuess = 0
 
-function randomizer() {
+ function randomizer() {
   // get number type in by user and convert to an integer
-userGuess = document.getElementById('input').value
+userGuess = document.getElementById('input')  .value
 userGuess = parseInt(userGuess)
 
-// generate random number between 1 and 6
+// generate random number between 1 and 6           
 randomNumber = (Math.random() * 6) + 1
 randomNumber = parseInt(randomNumber)
 
 // compare user's guess with random number
 if (userGuess === randomNumber) {
-document.getElementById('answer').innerHTML =     "you win!"
+document.getElementById('answer').innerHTML =         "you win!"
   }
 }
